5dc8a268482460858be6670b3537d62f577b6d38
ciro <lanubedethom@gmail.com>
2024-03-07 16:09:32 -0500

    hasta aqui corrige el error del sidebar que te aparecia cuando refrescabas la pagina. pagelout.jsx modificado

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,13 +1,20 @@
 import { Box, Flex, calc } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
 import SideBar from "../components/SideBar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/firebase";
 
 const PageLayout = ({ children }) => {
     const { pathname } = useLocation();
+    // te dice si el usuario esta autenticado o no
+    const [user, loading, error] = useAuthState(auth);
+    // si el usuario esta autenticado y no esta en la pagina de autenticacion, 
+    // mostrar el sidebar
+    const canRenderSidebar = pathname !== '/auth' && user;
     return (
         <Flex>
             {/*sidebar en la izquierda*/}
-            {pathname !== '/auth' ? (
+            {canRenderSidebar ? (
                 <Box w={{ base: '70px', md: '240px' }}>
                     <SideBar />
                 </Box>
