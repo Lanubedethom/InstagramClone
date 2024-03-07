9670a6b1ff1eff83341b065ff1e63c5afc0b7aac
ciro <lanubedethom@gmail.com>
2024-03-07 17:18:06 -0500

    hasta aqui se hizo un navbar de registro. Te muestra un profile de alguien con navbar de registro sin sidebar. Pagelayout modficado.

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,18 +1,34 @@
-import { Box, Flex, calc } from "@chakra-ui/react";
+import { Box, Flex, Spinner, calc } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
 import SideBar from "../components/SideBar";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
+import Navbar from "../components/Navbar";
+
+
+const PageLayoutSpinner = () => {
+    return (
+        <Flex flexDir={'column'} h={'100vh'} alignItems={'center'} justifyContent={'center'}>
+            <Spinner size={'xl'} />
+        </Flex>
+    )
+}
 
 const PageLayout = ({ children }) => {
     const { pathname } = useLocation();
     // te dice si el usuario esta autenticado o no
-    const [user, loading, error] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     // si el usuario esta autenticado y no esta en la pagina de autenticacion, 
     // mostrar el sidebar
     const canRenderSidebar = pathname !== '/auth' && user;
+    const canRenderNavbar = !user && !loading && pathname !== '/auth';
+
+
+    const checkingUserIsAuth = !user && loading;
+    if (checkingUserIsAuth) return <PageLayoutSpinner />;
+
     return (
-        <Flex>
+        <Flex flexDir={canRenderNavbar ? 'column' : 'row'}>
             {/*sidebar en la izquierda*/}
             {canRenderSidebar ? (
                 <Box w={{ base: '70px', md: '240px' }}>
@@ -20,9 +36,12 @@ const PageLayout = ({ children }) => {
                 </Box>
             ) : null}
 
+            {/*navbar*/}
+            {canRenderNavbar ? <Navbar /> : null}
+
 
             {/*contenido a la derecha*/}
-            <Box flex={1} w={{ base: 'calc(100%-70px)', md: 'calc(100%-240px)' }}>
+            <Box flex={1} w={{ base: 'calc(100%-70px)', md: 'calc(100%-240px)' }} mx={'auto'}>
                 {children}
             </Box>
         </Flex>
